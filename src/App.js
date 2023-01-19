21cd48f999a0cab6c7a44b487d5c4848f570f980
Idan Kazam <48420836+IDAN2468D@users.noreply.github.com>
2023-01-20 00:24:25 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Pokimon from "../src/Pokimon";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Pokimon />
+    </div>
+  );
+}
---
+++ src/Pokimon.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+export default function PokimonSearch() {
+  const [searchInput, setSearchInput] = useState();
+
+  const {
+    execute: fetchPokemon,
+    status,
+    error,
+    data: pokemonImg
+  } = useAsync(() => fetch(`https://pokeapi.co/api/v2/pokemon/${searchInput}`));
+
+  const onSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  return (
+    <div className="App">
+      <h1>Search for a pokemon</h1>
+      <input type="text" onClick={onSearchInputChange}></input>
+      <button onClick={fetchPokemon}>Search</button>
+      {status === "Loading" ? "Loading..." : null}
+      {status === "error" ? error : null}
+      {status === "success" && pokemonImg && (
+        <div>
+          <img src={pokemonImg.sprites.front_default} alt="pokemon" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+function useAsync(fn) {
+  const [data, setData] = useState(null);
+  const [status, setStatus] = useState("idle");
+  const [error, setError] = useState(null);
+
+  const execute = () => {
+    setStatus("loading");
+    setError(null);
+    fn()
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setStatus("success");
+      })
+      .catch((e) => {
+        setError(e.message);
+        setStatus("error");
+      });
+  };
+  return { execute, data, status, error };
+}
